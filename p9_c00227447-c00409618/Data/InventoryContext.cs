2a87ba34ed2b3b21c81c1e876d8af8f99c47bb8c
Jahzah <c00227447@louisiana.edu>
2022-04-23 20:46:52 -0500

    Initial Commit

---
+++ p9_c00227447-c00409618/Data/InventoryContext.cs
@@ -0,0 +1,17 @@
+﻿// Jahzah Jenkins, Tomas Parker
+// c00227447 c00409618
+// CMPS 358 
+// project #9
+
+using Microsoft.EntityFrameworkCore;
+
+namespace p9_c00227447_c00409618.Data;
+
+public class InventoryContext : DbContext
+{
+    public InventoryContext(DbContextOptions<InventoryContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<Models.InventoryItem> InventoryItems { get; set; }
+}
---
+++ p9_c00227447-c00409618/Models/InventoryItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace p9_c00227447_c00409618.Models;
+// Jahzah Jenkins, Tomas Parker
+// c00227447 c00409618
+// CMPS 358 
+// project #9
+public class InventoryItem
+{
+    public int Id { get; set; }
+    public string ItemName { get; set; }
+    public string Location { get; set; }
+    public float Value { get; set; }
+    
+}
